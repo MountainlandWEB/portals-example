724cf99f7d37c90673db3a6efcf54304b51b1845
Trevor Michaelis <trevor.michaelis@vivint.com>
2020-06-24 09:13:40 -0600

    tm: initial commit for react portals example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './styles.css'
+
+class Modal extends React.Component{
+  el = document.createElement('div')
+
+  componentDidMount() {
+    document.querySelector('#portal').appendChild(this.el)
+  }
+  
+  componentWillUnmount() {
+    document.querySelector('#portal').removeChild(this.el)
+  }
+
+  render() {
+    return ReactDOM.createPortal(
+      this.props.children,
+      this.el
+    )
+  }
 }
 
-export default App;
+class App extends React.Component {
+  state = {
+    showModal: false
+  }
+
+  handleOpenModal = () => {
+    this.setState({
+      showModal: true
+    })
+  }
+  handleCloseModal = () => {
+    this.setState({
+      showModal: false
+    })
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <button className="ui button primary" onClick={this.handleOpenModal}>Open</button>
+        { this.state.showModal ? (
+          <Modal>
+            <div className="modal">
+              <div className="ui cards">
+                <div className="card">
+                  <div className="content">
+                    <div className="header">
+                      Are you sure?
+                    </div>
+                    <div className="description">
+                      This action can not be undone.
+                    </div>
+                  </div>
+                  <div className="extra content">
+                    <div className="ui two buttons">
+                      <div className="ui basic button" onClick={this.handleCloseModal}>Cancel</div>
+                      <div className="ui basic primary button" onClick={this.handleCloseModal}>Continue</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Modal>
+        ) : null }
+      </div>
+    )
+  }
+}
+
+export default App
